ad5557ee4f7a7f5221daaa12d142cf7df369f5db
nullP01nterException <jolina.lam@sbcglobal.net>
2017-10-18 21:50:03 -0700

    implemented select and expand; implementation accuracy is questionable

--- mcts_vanilla.py
+++ mcts_vanilla.py
@@ -6,7 +6,7 @@ from math import sqrt, log
 num_nodes = 1000
 explore_faction = 2.
 
-def traverse_nodes(node, state, identity):
+def traverse_nodes(node, state, identity, board):
     """ Traverses the tree until the end criterion are met.
 
     Args:
@@ -17,11 +17,20 @@ def traverse_nodes(node, state, identity):
     Returns:        A node from which the next stage of the search can proceed.
 
     """
-    pass
+    leaf_node = None
+    best_value = 0  #holds value for best path based on wins/visits
+    child_nodes = node.child_nodes
+
+    if len(child_nodes) != 0:
+        for child in child_nodes:
+            if child.visits != 0 and child.wins/child.visits > best_value:
+                leaf_node = child
+
+    return leaf_node
     # Hint: return leaf_node
 
 
-def expand_leaf(node, state):
+def expand_leaf(node, board, state):
     """ Adds a new leaf to the tree by creating a new child node for the given node.
 
     Args:
@@ -31,11 +40,12 @@ def expand_leaf(node, state):
     Returns:    The added child node.
 
     """
-    pass
+    new_node = MCTSNode(parent=node, parent_action=node.parent_action, action_list=board.legal_actions(state))
+    return new_node
     # Hint: return new_node
 
 
-def rollout(state):
+def rollout(state, board):
     """ Given the state of the game, the rollout plays out the remainder randomly.
 
     Args:
@@ -45,7 +55,7 @@ def rollout(state):
     pass
 
 
-def backpropagate(node, won):
+def backpropagate(node, won, board):
     """ Navigates the tree from a leaf node to the root, updating the win and visit count of each node along the path.
 
     Args:
@@ -53,10 +63,12 @@ def backpropagate(node, won):
         won:    An indicator of whether the bot won or lost the game.
 
     """
+
     pass
 
 
 def think(board, state):
+    print("state", state)
     """ Performs MCTS by sampling games and calling the appropriate functions to construct the game tree.
 
     Args:
@@ -67,6 +79,7 @@ def think(board, state):
 
     """
     identity_of_bot = board.current_player(state)
+
     root_node = MCTSNode(parent=None, parent_action=None, action_list=board.legal_actions(state))
 
     for step in range(num_nodes):
@@ -77,7 +90,8 @@ def think(board, state):
         node = root_node
 
         # Do MCTS - This is all you!
-
+        #start work here with mcts
+        #traverse_nodes(node, state, identity_of_bot)
     # Return an action, typically the most frequently used action (from the root) or the action with the best
     # estimated win rate.
     return None
